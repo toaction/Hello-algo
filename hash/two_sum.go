4803885c51e2f728c3bdf56f11d188b43f512b0f
xiangfu <liuxiangfu2002@163.com>
2025-11-26 19:30:11 +0800

    chore: add problem-solving approaches to hash

--- hash/two_sum.go
+++ hash/two_sum.go
@@ -12,6 +12,15 @@ package hash
 //	输入：nums = [2,7,11,15], target = 9
 //	输出：[0,1]
 //	解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+//
+// 解题思路：
+//	- 哈希表存储：key = 数字值，value = 对应索引
+//	- 单次遍历：遍历数组时查找 complement = target - nums[i]
+//	- 快速查找：如果 complement 在哈希表中存在，说明找到答案
+//	- 逐步构建：如果 complement 不存在，将当前数字及其索引存入哈希表
+//
+// 时间复杂度：O(n) - 单次遍历数组
+// 空间复杂度：O(n) - 最坏情况下需要存储所有元素
 func TwoSum(nums []int, target int) []int {
 	// key = num, value = idx
 	ht := map[int]int{}
