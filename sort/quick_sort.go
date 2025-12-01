cb26200dfb987de57be26992b3555b2832d1ff9c
xiangfu <liuxiangfu2002@163.com>
2025-12-01 09:44:51 +0800

    chore: update quick sort partition style to hoare

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -6,28 +6,33 @@ func QuickSort(arr []int, left int, right int) {
 	if left >= right {
 		return
 	}
-	mid := partition(arr, left, right)
-	QuickSort(arr, left, mid-1)
-	QuickSort(arr, mid+1, right)
+	piv := partition(arr, left, right)
+	QuickSort(arr, left, piv)
+	QuickSort(arr, piv+1, right)
 }
 
-// 哨兵划分
+// Hoare 划分
 func partition(arr []int, left int, right int) int {
 	// 以 arr[left] 作为哨兵
-	i, j := left, right
+	pivot := arr[left]
+	i, j := left-1, right+1
 	for i < j {
-		// 最后要将 arr[i] 与哨兵交换，arr[i] 最后的值一定要 <= 哨兵，因此先从右向左遍历找到小于哨兵的元素
-		for i < j && arr[j] >= arr[left] {
-			j--
-		}
-		for i < j && arr[i] <= arr[left] {
+		for {
 			i++
+			if arr[i] >= pivot {
+				break
+			}
+		}
+		for {
+			j--
+			if arr[j] <= pivot {
+				break
+			}
 		}
 		if i < j {
 			arr[i], arr[j] = arr[j], arr[i]
 		}
-
 	}
-	arr[i], arr[left] = arr[left], arr[i]
-	return i
+	// 左区间 <= pivot，右区间 >= pivot，但划分点（左区间的右端点）不一定在最终位置
+	return j
 }
