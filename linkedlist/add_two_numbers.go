578eb2f01c2fb25983b58752ac31739e9733612b
xiangfu <liuxiangfu2002@163.com>
2025-12-09 21:42:07 +0800

    chore: update two-sum and remove-n-end

--- linkedlist/add_two_numbers.go
+++ linkedlist/add_two_numbers.go
@@ -1,18 +1,21 @@
 package linkedlist
 
-// https://leetcode.cn/problems/add-two-numbers/
-//
-// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
-// 请你将两个数相加，并以相同形式返回一个表示和的链表。
-// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-//
-// 示例：
-//
-//	输入：l1 = [2,4,3], l2 = [5,6,4]
-//	输出：[7,0,8]
-//	解释：342 + 465 = 807
+/* 
+2. 两数相加：https://leetcode.cn/problems/add-two-numbers/
+
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+	输入：l1 = [2,4,3], l2 = [5,6,4]
+	输出：[7,0,8]
+	解释：342 + 465 = 807 
+*/
+
+// 模拟计算
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	dump := &ListNode{Val: 0}
+	dump := &ListNode{}
 	cur := dump
 	carry := 0
 	// 共有的低位
@@ -27,10 +30,8 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		l2 = l2.Next
 	}
 	// 剩余的高位
-	var high *ListNode
-	if l1 != nil {
-		high = l1
-	} else {
+	high := l1
+	if l2 != nil {
 		high = l2
 	}
 	for high != nil {
--- linkedlist/remove_nth_from_end.go
+++ linkedlist/remove_nth_from_end.go
@@ -1,33 +1,34 @@
 package linkedlist
 
-// https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
-//
-// 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
-//
-// 示例：
-//
-//	输入：head = [1,2,3,4,5], n = 2
-//	输出：[1,2,3,5]
-//	解释：删除了倒数第 2 个节点 4
-//
-// 思路：快慢双指针（一次遍历）
+/*
+19. 删除链表的倒数第 N 个结点：https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+示例：
+	输入：head = [1,2,3,4,5], n = 2
+	输出：[1,2,3,5]
+	解释：删除了倒数第 2 个节点 4
+*/
+
+// 双指针
 func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
-	k := n
-	dump := &ListNode{Val: 0, Next: head}
-	cur := head
-	for cur != nil && k > 0 {
-		cur = cur.Next
-		k--
+	dump := &ListNode{Next: head}
+	// 要删除节点的前一个节点、链表的尾节点
+	pre, end := dump, head
+	i := 0
+	for i = 0; i < n && end != nil; i++ {
+		end = end.Next
 	}
-	if k > 0 {
+	if i != n {
 		return nil
 	}
-	// 要删除节点的前一个结点
-	ahead := dump
-	for cur != nil {
-		cur = cur.Next
-		ahead = ahead.Next
+	// 双指针同时移动
+	for end != nil {
+		pre = pre.Next
+		end = end.Next
 	}
-	ahead.Next = ahead.Next.Next
+	// 删除节点
+	pre.Next = pre.Next.Next
 	return dump.Next
 }
